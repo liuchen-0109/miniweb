7b4dc8caa8b3351cf6ecdb182e3c427fd0911620
liuchen <2354921286@qq.com>
2019-01-21 11:07:00 +0800

    1

---
+++ app.js
@@ -0,0 +1,31 @@
+App({
+    // onLaunch: function () {
+    //     wx.login({
+    //         success: function (res) {
+    //             if (res.code) {
+    //                 //发起网络请求
+    //                 wx.request({
+    //                     url: 'https://www.turbochen.xyz/wen',
+    //                     data: {
+    //                         code: res.code
+    //                     },
+    //                     success:function (res) {
+    //                       wx.getUserInfo({
+    //                         success:function(info){
+    //                             console.log(info)
+    //                         }
+    //                       })
+    //                     }
+    //                 })
+    //             } else {
+    //                 console.log('获取用户登录态失败！' + res.errMsg)
+    //             }
+    //         }
+    //
+    //
+    //     });
+    //
+    // },
+
+
+})
---
+++ pages/index/index.js
@@ -0,0 +1,48 @@
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var util = require('../../utils/util.js')
+var config = require('../../config')
+qcloud.setLoginUrl(config.service.loginUrl)
+
+
+Page({
+    data: {
+        userInfo: {},
+        logged: false,
+        takeSession: false,
+        requestResult: ''
+    },
+
+    onLoad: function () {
+        if (this.data.logged) return
+
+        util.showBusy('正在登录')
+
+        const session = qcloud.Session.get()
+
+        if (session) {
+            qcloud.loginWithCode({
+                success: res => {
+                    this.setData({ userInfo: res, logged: true })
+                    util.showSuccess('登录成功')
+                },
+                fail: err => {
+                    console.error(err)
+                    util.showModel('登录错误', err.message)
+                }
+            })
+        } else {
+            qcloud.login({
+                success: res => {
+                    this.setData({ userInfo: res, logged: true })
+                    util.showSuccess('登录成功')
+                },
+                fail: err => {
+                    console.error(err)
+                    util.showModel('登录错误', err.message)
+                }
+            })
+        }
+    },
+
+
+})
